b74e88b995bda0e8b8c7c2da9129f12314c33c81
VaLpEr <valper91@gmail.com>
2019-01-14 15:28:36 +0500

    Initial commit

---
+++ src/Sapper.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Sapper extends JFrame {
+
+    private JPanel panel;
+
+    public static void main(String[] args) {
+        new Sapper();
+    }
+
+// Конструктор основного класса Sapper где создаеться основной GUI
+    private Sapper(){
+        initPanel();
+        initFrame();
+    }
+
+// Метод по инициализации панели (JPanel)
+    private void initPanel() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500,300)); //  установка размера панели с использование менеджера компановки
+        add(panel);
+    }
+
+// Метод по инициализации фрейма JFrame
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Сапер");
+        setVisible(true);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+    }
+}
